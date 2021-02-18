2fe70606dd7fb482f0edcb9c41a6e444fab9058f
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-18 09:34:35 -0500

    logica en .jsx con el return html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Header() {
 
+    const edad = 19;
+    let mensaje;
+    if(edad == 18){
+        mensaje = 'el numero es 18';
+    }else{
+        mensaje = 'el numero no es 18'
+    }
     return (
-        <h1>Desde el header De nuevo</h1>
+        <h1>El numero:{edad} {mensaje}</h1>
     )
     
 }
