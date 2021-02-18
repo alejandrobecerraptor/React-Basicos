a1a86fad8a1daa0dc8d98eded0472fcd2d73494f
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-18 13:39:33 -0500

    Evento onClick React

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -4,10 +4,23 @@ const Producto = ({producto}) => {
 
     const {nombre , precio , id} = producto;
 
+    //Agregar producto al carrito funcion
+
+    const seleccionarProducto = id => {
+        return ( 
+            console.log('comprando',id)
+         );
+    }
+     
     return ( 
        <div>
            <h2>{nombre}</h2>
            <p>${precio}</p>
+           <button
+           type = "button"
+           onClick = { () => seleccionarProducto(id) }>
+               comprar
+           </button>
        </div>
      );
 }
