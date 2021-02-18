b05997cac3c8d1de8e3e7b5ee17a4f8ce3643622
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-18 12:57:52 -0500

    Hooks: explicacion en el video, y creacion de un useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { Fragment } from 'react';
+import { Fragment,useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 
 function App() {
 
+  //crear un listado de productos
+  // asi se crean los state
+  const [productos, guardarProductos] = useState([ 
+    { id: 1, nombre:'camisa react',precio: 50 },
+    { id: 2, nombre:'camisa laravel',precio: 40 },
+    { id: 3, nombre:'camisa node',precio: 30 },
+    { id: 4, nombre:'camisa php',precio: 20 },
+  ]); 
+
+
+  //obtener fecha
   const fecha = new Date().getFullYear();
 
   return (
