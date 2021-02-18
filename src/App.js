fbfb51a5788fd65f78a1114e061cab6a52a3c89c
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-18 13:11:39 -0500

    Componente que lista unos productos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment,useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Producto from './components/Producto';
 
 function App() {
 
@@ -23,7 +23,19 @@ function App() {
       <Header 
         titulo = 'Tienda Virtual'
       />
-          
+
+      <h1>Lista de Productos:</h1>
+      {productos.map(producto => (
+        <Producto
+          key = {producto.id}
+          producto={producto}
+        />
+      ))}
+
+
+
+
+
       <Footer 
         fecha = {fecha}
       />
